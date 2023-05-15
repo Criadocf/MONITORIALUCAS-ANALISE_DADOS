1981468ff1e5d01f8e2259e0d3e41eb640bac83f
Kleberjunior <kleberjrmmxvi@gmail.com>
2023-05-15 17:09:49 -0300

    Mais algumas questoes do data-set carros

--- data_set_carros.py
+++ data_set_carros.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 car = pd.read_csv('carros.csv', sep=';')
 
-print(car.head(5))
+print(car.head(200))
 
 print('------------------------//------------------------//-------------------//--------------')
 
@@ -40,4 +40,26 @@ print('----------//------------------//------------------------')
 print('')
 print('MAIS INFORMAÇÕES DO DATASET COM A FUNÇÃO INFO().')
 print(car.info())
+
+print("""8- Com base no Dataset que estamos trabalhando, defina uma função para mostrar a quilometragem media de todos os carros
+...sabendo que a formula é: km_media = km_total / (ano_atual - ano_fabricacao) /// A função tem que receber como parâmetros: o 
+dataset e o ano atual. Execute a funçaõ e mostre o resultado.""")
+
+#def quilometragem_media(dt_set, anno_atual):
+#    ler_data = pd.read_csv()
+
+print('PESQUISA COM QUERIES', 'a)mostre os carros com motor "Diesel V8" ', sep="\n")
+
+print(car.loc[car['Motor'] == 'Motor Diesel V8'])
+
+print('b) Carros com motor 1.0 8v e com Valor menor que R$100.000')
+b = (car['Motor'] == 'Motor 1.0 8v') & (car['Valor'] < 100000)
+b_filtro = car.loc[b]
+print(b_filtro)
+
+print()
+
+
+
+
 
